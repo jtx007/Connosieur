534e0e3fa2915a2718a744e8084986f48ea8367a
jtx007 <jamesjacobthomas7@gmail.com>
2020-04-11 15:15:22 -0400

    changed footer link color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
       <div className="content has-text-centered">
         <p className="">
           <strong>Connosieur</strong> by{" "}
-          <a href="https://jamesjacobthomas.com">James Thomas </a>
-           © 2019
+          <a className="footerLink" href="https://jamesjacobthomas.com">James Thomas </a>
+           © 2020
         </p>
-          <div className="footerLinks">
-            <div><a  href="https://github.com/jtx007"><i className="fab fa-github is-size-1"></i></a></div>
-            <div><a  href="https://www.linkedin.com/in/james-thomas007/"><i className="fab fa-linkedin is-size-1"></i></a></div>
-            <div><a  href="mailto:jamesjacobthomas7@gmail.com"><i className="fas fa-envelope is-size-1"></i></a></div>
+          <div className="footerLinksContainer">
+            <div><a className="footerLink"  href="https://github.com/jtx007"><i className="fab fa-github is-size-1"></i></a></div>
+            <div><a className="footerLink"  href="https://www.linkedin.com/in/james-thomas007/"><i className="fab fa-linkedin is-size-1"></i></a></div>
+            <div><a className="footerLink" href="mailto:jamesjacobthomas7@gmail.com"><i className="fas fa-envelope is-size-1"></i></a></div>
           </div>
       </div>
     </footer>
